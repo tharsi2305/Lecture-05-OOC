c4bcd7ce0b111c9b51cf9a5dfe041b0e5b0db46a
Tharsika <99468178+tharsi2305@users.noreply.github.com>
2022-02-22 11:22:26 +0000

    L5

--- .ccls-cache/@home@runner@GrimyFlatEnvironment/Main.cpp
+++ .ccls-cache/@home@runner@GrimyFlatEnvironment/Main.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main() {
   Student std1, std2;
-  std1.setStudentNo(1023);
+  std1.setStudentNo(11);
   std1.assignMarks(50,60,70);
-  std2.setStudentNo(2345);
+  std2.setStudentNo(33);
   std2.assignMarks(70,80,75);
+  //cout <<"Average of student1:"<< std1.setStudentNo(1023)<< endl;
   cout <<"Average of student1:"<< std1.calcAvg() << endl;
+  //cout<<std1.setStudentNo();
   cout <<"Average of student2:"<< std2.calcAvg() << endl;
   return 0;
+  
 }
 
